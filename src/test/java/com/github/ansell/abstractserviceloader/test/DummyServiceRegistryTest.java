3482c2376d981b26991c7df79dc5867207ad4e9f
Peter Ansell <p_ansell@yahoo.com>
2015-03-29 11:40:24 +1100

    test the class loader constructors

--- src/test/java/com/github/ansell/abstractserviceloader/test/DummyServiceRegistryTest.java
+++ src/test/java/com/github/ansell/abstractserviceloader/test/DummyServiceRegistryTest.java
@@ -12,41 +12,36 @@ import com.github.ansell.abstractserviceloader.AbstractServiceLoader;
  * 
  * @author Peter Ansell p_ansell@yahoo.com
  */
-public class DummyServiceRegistryTest extends AbstractServiceLoaderTest<String, DummyService>
-{
-    @Override
-    public int getExpectedInitialServiceCount()
-    {
-        return 1;
-    }
-    
-    @Override
-    public Collection<String> getExpectedInitialServiceKeys()
-    {
-        return Arrays.asList("DummyServiceImpl");
-    }
-    
-    @Override
-    public String getExpectedKeyForNewService()
-    {
-        return "ManuallyCreatedDummyServiceKey";
-    }
-    
-    @Override
-    public DummyService getNewService()
-    {
-        return new DummyServiceImpl("ManuallyCreatedDummyServiceKey");
-    }
-    
-    @Override
-    public AbstractServiceLoader<String, DummyService> getNewServiceLoader()
-    {
-        return new DummyServiceRegistry();
-    }
+public class DummyServiceRegistryTest extends
+		AbstractServiceLoaderTest<String, DummyService> {
+	@Override
+	public int getExpectedInitialServiceCount() {
+		return 1;
+	}
 
 	@Override
-	public DummyService getNewServiceNullKey() 
-	{
+	public Collection<String> getExpectedInitialServiceKeys() {
+		return Arrays.asList("DummyServiceImpl");
+	}
+
+	@Override
+	public String getExpectedKeyForNewService() {
+		return "ManuallyCreatedDummyServiceKey";
+	}
+
+	@Override
+	public DummyService getNewService() {
+		return new DummyServiceImpl("ManuallyCreatedDummyServiceKey");
+	}
+
+	@Override
+	public AbstractServiceLoader<String, DummyService> getNewServiceLoader() {
+		return new DummyServiceRegistry(Thread.currentThread()
+				.getContextClassLoader());
+	}
+
+	@Override
+	public DummyService getNewServiceNullKey() {
 		return new DummyServiceImpl(null);
 	}
 }
--- src/test/java/com/github/ansell/abstractserviceloader/test/DummyUniqueServiceRegistryTest.java
+++ src/test/java/com/github/ansell/abstractserviceloader/test/DummyUniqueServiceRegistryTest.java
@@ -12,41 +12,36 @@ import com.github.ansell.abstractserviceloader.AbstractUniqueServiceLoader;
  * 
  * @author Peter Ansell p_ansell@yahoo.com
  */
-public class DummyUniqueServiceRegistryTest extends AbstractUniqueServiceLoaderTest<String, DummyService>
-{
-    @Override
-    public int getExpectedInitialServiceCount()
-    {
-        return 1;
-    }
-    
-    @Override
-    public Collection<String> getExpectedInitialServiceKeys()
-    {
-        return Arrays.asList("DummyServiceImpl");
-    }
-    
-    @Override
-    public String getExpectedKeyForNewService()
-    {
-        return "ManuallyCreatedDummyUniqueServiceKey";
-    }
-    
-    @Override
-    public DummyService getNewDuplicateService(final String key)
-    {
-        return new DummyServiceImpl(key);
-    }
-    
-    @Override
-    public AbstractUniqueServiceLoader<String, DummyService> getNewUniqueServiceLoader()
-    {
-        return new DummyUniqueServiceRegistry();
-    }
+public class DummyUniqueServiceRegistryTest extends
+		AbstractUniqueServiceLoaderTest<String, DummyService> {
+	@Override
+	public int getExpectedInitialServiceCount() {
+		return 1;
+	}
 
 	@Override
-	public DummyService getNewServiceNullKey() 
-	{
-        return new DummyServiceImpl(null);
+	public Collection<String> getExpectedInitialServiceKeys() {
+		return Arrays.asList("DummyServiceImpl");
+	}
+
+	@Override
+	public String getExpectedKeyForNewService() {
+		return "ManuallyCreatedDummyUniqueServiceKey";
+	}
+
+	@Override
+	public DummyService getNewDuplicateService(final String key) {
+		return new DummyServiceImpl(key);
+	}
+
+	@Override
+	public AbstractUniqueServiceLoader<String, DummyService> getNewUniqueServiceLoader() {
+		return new DummyUniqueServiceRegistry(Thread.currentThread()
+				.getContextClassLoader());
+	}
+
+	@Override
+	public DummyService getNewServiceNullKey() {
+		return new DummyServiceImpl(null);
 	}
 }
